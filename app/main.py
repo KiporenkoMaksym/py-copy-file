e6936c8fcbf51fbd61abd19ec62853b2e2823b1a
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-09-08 00:07:09 +0300

    Complete

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    str_split = command.split(" ")
+    if len(str_split) != 3:
+        return
+
+    action, source, target = str_split
+    if action != "cp":
+        return
+    if source == target:
+        return
+
+    try:
+        with open(source, "r") as file_in, open(target, "w") as file_out:
+            content = file_in.read()
+            file_out.write(content)
+    except FileNotFoundError:
+        return
